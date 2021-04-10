df1941faa4950ab8d194048a6a1c0a4e3a082d06
HugoPoletto34 <hugopoletto34@gmail.com>
2021-04-10 20:59:07 -0300

    Classe de Leitura de arquivo texto

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,6 +1,5 @@
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.*;
+
 
 public class Aplicacao {
     public static void main(String[] args) {
@@ -22,5 +21,47 @@ public class Aplicacao {
 
 
         arq.fecharArquivo();
+    }
+}
+
+class ArquivoTextoLeitura {
+
+    private BufferedReader entrada;
+
+    public void abrirArquivo(String nomeArquivo){
+
+        try {
+            entrada = new BufferedReader(new FileReader(nomeArquivo));
+        }
+        catch (FileNotFoundException excecao) {
+            System.out.println("Arquivo não encontrado");
+        }
+    }
+
+    public void fecharArquivo() {
+
+        try {
+            entrada.close();
+        }
+        catch (IOException excecao) {
+            System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);
+        }
+    }
+
+    public String ler() {
+
+        String textoEntrada;
+
+        try {
+            textoEntrada = entrada.readLine();
+        }
+        catch (EOFException excecao) { //Exceção de final de arquivo.
+            return null;
+        }
+        catch (IOException excecao) {
+            System.out.println("Erro de leitura: " + excecao);
+            return null;
+        }
+        return textoEntrada;
     }
 }
